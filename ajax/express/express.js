5fdcfd9e21a5dd8fa914ede40f55e250f19c3a2b
codesLive <2783779285@qq.com>
2023-04-10 22:45:33 +0800

    ajax案例准备
    Signed-off-by: codesLive <2783779285@qq.com>

---
+++ ajax/express/express.js
@@ -0,0 +1,16 @@
+//导入 express
+const express = require('express');
+
+//创建实例对象
+const app = express();
+
+//创建路由规则
+app.get('/server', (req, res)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.send('HELLO AJAX');
+});
+
+//监听端口
+app.listen(8000, ()=>{
+  console.log('服务已经启动, 8000端口正在运行在……');
+});
